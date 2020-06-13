a33f471a7bcba3d6e8b01e7d8fb30f693903d11b
yf855 <56779633+yf855@users.noreply.github.com>
2020-06-12 18:14:18 -0700

    Update preprocess_audio.py

--- utilitytools/preprocess_audio.py
+++ utilitytools/preprocess_audio.py
@@ -4,8 +4,10 @@ from pydub import AudioSegment
 import librosa
 import os
 
-folder_str = "/Users/fyy0194/Documents/cse237D/grabador2019_1"
-tar_str = "/Users/fyy0194/Documents/cse237D/grabador2019_1/output/"
+folder_str = sys.argv[1]
+tar_str = sys.argv[2]
+#folder_str = "/Users/fyy0194/Documents/cse237D/grabador2019_1"
+#tar_str = "/Users/fyy0194/Documents/cse237D/grabador2019_1/output/"
 for file in os.listdir(folder_str):
     audio_name = os.fsdecode(file)
     if audio_name.endswith(".WAV"):
